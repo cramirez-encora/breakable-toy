c96ae84314089d1d3305b9c7fb04dbe7c852ae68
cramirez-encora <cecilia.ramirez@encora.com>
2025-03-13 20:51:53 -0600

    backend initial config

---
+++ backend/src/test/java/backend/inventoryBackend/InventoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.inventoryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
